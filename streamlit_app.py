0647290f9edcdaa1836f4939d74281f6b14dcccb
saeedteymouri <129985059+saeedteymouri@users.noreply.github.com>
2023-09-14 12:35:43 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,12 @@
-import tkinter as tk
+# Create a list to store the names
+names = []
 
-class NameEntryForm(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
+# Use a for loop to prompt the user for 11 names
+for i in range(1, 12):
+    name = input(f"Enter name {i}: ")
+    names.append(name)
 
-        # Create 11 question boxes for names
-        self.name_boxes = []
-        for i in range(11):
-            self.name_boxes.append(tk.Entry(self))
-            self.name_boxes[i].grid(row=i, column=0)
-
-        # Create a submit button
-        self.submit_button = tk.Button(self, text="Submit", command=self.submit)
-        self.submit_button.grid(row=11, column=0)
-
-    def submit(self):
-        # Get the names from the question boxes
-        names = []
-        for name_box in self.name_boxes:
-            names.append(name_box.get())
-
-        # Print the names to the console
-        print("The names are:")
-        for name in names:
-            print(name)
-
-root = tk.Tk()
-name_entry_form = NameEntryForm(root)
-name_entry_form.pack()
-root.mainloop()
-
+# Print the collected names
+print("You entered the following names:")
+for name in names:
+    print(name)
