6882417176541095872bf9323e4f337b96ebd956
saeedteymouri <129985059+saeedteymouri@users.noreply.github.com>
2023-09-14 13:03:00 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,40 @@
 import streamlit as st
-# Create a list to store the names
-names = []
 
-# Use a for loop to prompt the user for 11 names
-for i in range(1, 12):
-    name = input(f"Enter name {i}: ")
-    names.append(name)
+# Create a list of default names
+default_names = [
+    "The Godfather",
+    "Matador",
+    "Saul Goodman",
+    "Dr. Watson",
+    "Leon",
+    "Constantine",
+    "Nostradamus",
+    "Citizen Kane",
+    "Simple Citizen 1",
+    "Simple Citizen 2",
+    "Simple Citizen 3"
+]
 
-# Print the collected names
-print("You entered the following names:")
-for name in names:
-    print(name)
-st.title("Godfother Moderator")
+# Create a Streamlit app
+def main():
+    st.title("Name List App")
+
+    # Create a sidebar with the default names
+    st.sidebar.header("Names")
+    selected_names = st.sidebar.multiselect("Select Names", default_names)
+
+    # Create an edit box for entering names
+    new_name = st.text_input("Enter a Name")
+
+    # Create a confirmation button
+    if st.button("Add Name"):
+        if new_name:
+            selected_names.append(new_name)
+
+    # Display the table with selected names
+    st.header("Selected Names")
+    data = {"Names": selected_names}
+    st.table(data)
+
+if __name__ == "__main__":
+    main()
