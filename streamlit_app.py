8faa5e5e87d51df6ae57ddd58641f5c7f94eece0
saeedteymouri <129985059+saeedteymouri@users.noreply.github.com>
2023-09-14 13:14:08 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,27 @@
 import streamlit as st
 
-# Create a list of default names
-default_names = [
-    "The Godfather",
-    "Matador",
-    "Saul Goodman",
-    "Dr. Watson",
-    "Leon",
-    "Constantine",
-    "Nostradamus",
-    "Citizen Kane",
-    "Simple Citizen 1",
-    "Simple Citizen 2",
-    "Simple Citizen 3"
-]
+# Create a dictionary with default roles and names
+default_data = {
+    "The Godfather": "",
+    "Matador": "",
+    "Saul Goodman": "",
+    "Dr. Watson": "",
+    "Leon": "",
+    "Constantine": "",
+    "Nostradamus": "",
+    "Citizen Kane": "",
+    "Simple Citizen 1": "",
+    "Simple Citizen 2": "",
+    "Simple Citizen 3": ""
+}
 
 # Create a Streamlit app
 def main():
     st.title("Name List App")
 
-    # Create a sidebar with the default names
-    st.sidebar.header("Names")
-    selected_name = st.sidebar.selectbox("Select a Name", default_names)
+    # Create a sidebar with the default roles
+    st.sidebar.header("Roles")
+    selected_role = st.sidebar.selectbox("Select a Role", list(default_data.keys()))
 
     # Create an edit box for entering names
     new_name = st.text_input("Enter a Name")
@@ -29,10 +29,12 @@ def main():
     # Create a confirmation button
     if st.button("Add Name"):
         if new_name:
-            st.write(f"{new_name} -------------------- {selected_name}")
+            default_data[selected_role] = new_name
 
-    # Display the table with selected names
-    st.header("Selected Names")
+    # Display the table with roles and names
+    st.header("Roles and Names")
+    data = {"Role": list(default_data.keys()), "Name": list(default_data.values())}
+    st.table(data)
 
 if __name__ == "__main__":
     main()
