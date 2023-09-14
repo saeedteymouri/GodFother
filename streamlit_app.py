cb3f2c2287d507b71334eec297596e2a0a55e8fb
saeedteymouri <129985059+saeedteymouri@users.noreply.github.com>
2023-09-14 18:56:22 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,36 +2,44 @@ import streamlit as st
 import pandas as pd
 import random
 
-# Create a list of characters
-characters = [
-    "Godfather", "Matador", "Saul Goodman", "Dr. Watson", "Leon",
-    "Constantine", "Nostradamus", "Citizen Kane", "Simple Citizen 1",
-    "Simple Citizen 2", "Simple Citizen 3"
-]
-
-# Initialize a dictionary to store names for each character
-if 'character_names' not in st.session_state:
-    st.session_state.character_names = {char: "" for char in characters}
-
 # Create a Streamlit app
 def main():
     st.title("Character Name List App")
 
-    # Create input boxes for entering names with labels "Person 1" to "Person 11"
+    # Ask the user for the number of players
+    num_players = st.selectbox("Select the Number of Players", list(range(11, 14)))
+
+    # Create a list of characters based on the selected number of players
+    characters = [
+        "Godfather", "Matador", "Saul Goodman", "Dr. Watson", "Leon",
+        "Constantine", "Nostradamus", "Citizen Kane", "Simple Citizen 1",
+        "Simple Citizen 2", "Simple Citizen 3"
+    ]
+
+    if num_players >= 12:
+        characters.append("Simple Citizen 4")
+    if num_players == 13:
+        characters.append("Simple Mafia")
+
+    # Initialize a dictionary to store names for each character
+    if 'character_names' not in st.session_state:
+        st.session_state.character_names = {char: "" for char in characters}
+
+    # Create input boxes for entering names with labels "Person 1" to "Person n"
     st.header("Enter Names:")
     for i, char in enumerate(characters):
         st.session_state.character_names[char] = st.text_input(f"Person {i+1}:", st.session_state.character_names[char])
 
     # Create a button to assign roles
     if st.button("Assign Role"):
-        assign_roles()
+        assign_roles(characters)
 
     # Display the table only after assigning roles
     if any(st.session_state.character_names.values()):
         df = pd.DataFrame({"Character": characters, "Name": [st.session_state.character_names[char] for char in characters]})
         st.table(df.set_index("Character"))
 
-def assign_roles():
+def assign_roles(characters):
     # Shuffle the names list to randomize the names
     names = list(st.session_state.character_names.values())
     random.shuffle(names)
