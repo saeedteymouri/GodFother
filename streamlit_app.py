68567dde78bc5793af514c413e1148bf01fb5a91
saeedteymouri <129985059+saeedteymouri@users.noreply.github.com>
2023-09-15 08:58:01 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,15 +13,24 @@ characters = [
 if 'character_names' not in st.session_state:
     st.session_state.character_names = {char: "" for char in characters}
 
-# Initialize Godfather's and Matador's abilities and victims
-if 'godfather_ability' not in st.session_state:
-    st.session_state.godfather_ability = "doesn't kill anyone"
+# Initialize Godfather's and Matador's abilities and victims for Night 1 and Night 2
+if 'godfather_ability_1' not in st.session_state:
+    st.session_state.godfather_ability_1 = "doesn't kill anyone"
 
-if 'godfather_victim' not in st.session_state:
-    st.session_state.godfather_victim = ""
+if 'godfather_victim_1' not in st.session_state:
+    st.session_state.godfather_victim_1 = ""
 
-if 'matador_victim' not in st.session_state:
-    st.session_state.matador_victim = ""
+if 'matador_victim_1' not in st.session_state:
+    st.session_state.matador_victim_1 = ""
+
+if 'godfather_ability_2' not in st.session_state:
+    st.session_state.godfather_ability_2 = "doesn't kill anyone"
+
+if 'godfather_victim_2' not in st.session_state:
+    st.session_state.godfather_victim_2 = ""
+
+if 'matador_victim_2' not in st.session_state:
+    st.session_state.matador_victim_2 = ""
 
 # Create a Streamlit app
 def main():
@@ -41,22 +50,37 @@ def main():
         df = pd.DataFrame({"Character": characters, "Name": [st.session_state.character_names[char] for char in characters]})
         st.table(df.set_index("Character"))
 
-    # Section for Godfather's ability
+    # Section for Night 1
     st.header("During the Night 1")
-    st.subheader("The Role of the Godfather")
-    godfather_ability = st.selectbox("Choose Godfather's Ability:", ["doesn't kill anyone", "kills", "slaughters"])
-    st.session_state.godfather_ability = godfather_ability
-    godfather_victim = st.text_input("Enter Victim's Name (if applicable):")
-    st.session_state.godfather_victim = godfather_victim
+    st.subheader("The Role of the Godfather 1")
+    godfather_ability_1 = st.selectbox("Choose Godfather's Ability:", ["doesn't kill anyone", "kills", "slaughters"])
+    st.session_state.godfather_ability_1 = godfather_ability_1
+    godfather_victim_1 = st.text_input("Enter Victim's Name (if applicable):")
+    st.session_state.godfather_victim_1 = godfather_victim_1
 
-    # Section for Matador's ability
-    st.subheader("The Role of the Matador")
-    matador_victim = st.text_input("Enter Matador's Target's Name (if applicable):")
-    st.session_state.matador_victim = matador_victim
+    st.subheader("The Role of the Matador 1")
+    matador_victim_1 = st.text_input("Enter Matador's Target's Name (if applicable):")
+    st.session_state.matador_victim_1 = matador_victim_1
 
-    # Button to display ability actions
-    if st.button("Ability Actions"):
-        display_ability_actions()
+    # Button to display ability actions for Night 1
+    if st.button("Ability Actions 1"):
+        display_ability_actions(1)
+
+    # Section for Night 2
+    st.header("During the Night 2")
+    st.subheader("The Role of the Godfather 2")
+    godfather_ability_2 = st.selectbox("Choose Godfather's Ability:", ["doesn't kill anyone", "kills", "slaughters"])
+    st.session_state.godfather_ability_2 = godfather_ability_2
+    godfather_victim_2 = st.text_input("Enter Victim's Name (if applicable):")
+    st.session_state.godfather_victim_2 = godfather_victim_2
+
+    st.subheader("The Role of the Matador 2")
+    matador_victim_2 = st.text_input("Enter Matador's Target's Name (if applicable):")
+    st.session_state.matador_victim_2 = matador_victim_2
+
+    # Button to display ability actions for Night 2
+    if st.button("Ability Actions 2"):
+        display_ability_actions(2)
 
 def assign_roles():
     # Shuffle the names list to randomize the names
@@ -67,10 +91,10 @@ def assign_roles():
     df = pd.DataFrame({"Character": characters, "Name": names})
     st.session_state.character_names = {char: name for char, name in zip(characters, names)}
 
-def display_ability_actions():
-    godfather_ability = st.session_state.godfather_ability
-    godfather_victim = st.session_state.godfather_victim
-    matador_victim = st.session_state.matador_victim
+def display_ability_actions(night):
+    godfather_ability = st.session_state[f'godfather_ability_{night}']
+    godfather_victim = st.session_state[f'godfather_victim_{night}']
+    matador_victim = st.session_state[f'matador_victim_{night}']
 
     matador_ability_message = ""
 
@@ -81,16 +105,4 @@ def display_ability_actions():
         st.write("The Godfather doesn't kill anyone during the night.")
     elif godfather_ability == "kills":
         if godfather_victim:
-            st.write(f"The Godfather kills {godfather_victim} during the night.")
-        else:
-            st.write("The Godfather kills someone during the night.")
-    elif godfather_ability == "slaughters":
-        if godfather_victim:
-            st.write(f"The Godfather slaughters {godfather_victim} during the night.")
-        else:
-            st.write("The Godfather slaughters someone during the night.")
-    
-    st.write(matador_ability_message)
-
-if __name__ == "__main__":
-    main()
+            st.write(f"The Godfather kills {godfather_victim} during
