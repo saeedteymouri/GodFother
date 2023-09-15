9dcd7fe6d708e03788e0498d43b76f983624a97d
saeedteymouri <129985059+saeedteymouri@users.noreply.github.com>
2023-09-15 23:28:27 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,113 +1,13 @@
-import streamlit as st
-import pandas as pd
-import random
-
-# Create a list of characters
-characters = [
-    "Godfather", "Matador", "Saul Goodman", "Dr. Watson", "Leon",
-    "Constantine", "Nostradamus", "Citizen Kane", "Simple Citizen 1",
-    "Simple Citizen 2", "Simple Citizen 3"
-]
-
-# Define the sides associated with characters
-character_sides = {
-    "Godfather": "Mafia",
-    "Matador": "Mafia",
-    "Saul Goodman": "Mafia",
-    "Dr. Watson": "Citizen",
-    "Leon": "Citizen",
-    "Constantine": "Citizen",
-    "Nostradamus": "Independent",
-    "Citizen Kane": "Citizen",
-    "Simple Citizen 1": "Citizen",
-    "Simple Citizen 2": "Citizen",
-    "Simple Citizen 3": "Citizen"
-}
-
-# Initialize a dictionary to store names for each character
-if 'character_names' not in st.session_state:
-    st.session_state.character_names = {char: "" for char in characters}
-
-# Initialize a dictionary to store night-specific data
-if 'night_data' not in st.session_state:
-    st.session_state.night_data = {}
-
-# Create a Streamlit app
-def main():
-    st.title("Character Name List App")
-
-    # Create input boxes for entering names with labels "Person 1" to "Person 11"
-    st.header("Enter Names:")
-    for i, char in enumerate(characters):
-        st.session_state.character_names[char] = st.text_input(f"Person {i+1}:", st.session_state.character_names[char])
-
-    # Create a button to assign roles
-    if st.button("Assign Role"):
-        assign_roles()
-
-    # Display the table of names and roles
-    if any(st.session_state.character_names.values()):
-        df = pd.DataFrame({"Character": characters, "Name": [st.session_state.character_names[char] for char in characters]})
-        st.table(df.set_index("Character"))
-
-    # Input box for entering a person's name
-    person_name = st.text_input("Enter Person's Name:")
-    if st.button("Show Person's Role"):
-        show_person_role(person_name)
-
-    # Section for Night 1
-    display_night_section(1)
-
-    # Section for Night 2
-    display_night_section(2)
-
-def assign_roles():
-    # Shuffle the names list to randomize the names
-    names = list(st.session_state.character_names.values())
-    random.shuffle(names)
-
-    # Assign randomized names to the table
-    df = pd.DataFrame({"Character": characters, "Name": names})
-    st.session_state.character_names = {char: name for char, name in zip(characters, names)}
-
-def get_person_role_by_name(person_name):
-    person_role = ""
-    for char, name in st.session_state.character_names.items():
-        if name.lower() == person_name.lower():
-            person_role = char
-            break
-    return person_role
-
-def show_person_role(person_name):
-    person_role = get_person_role_by_name(person_name)
-    side = character_sides.get(person_role)
-    st.write(f"{person_name}'s role is {person_role} ({side})")
-
-def display_night_section(night):
-    st.header(f"During the Night {night}")
-    godfather_ability = st.selectbox(f"Choose Godfather's Ability (Night {night}):", ["doesn't kill anyone", "kills", "slaughters"])
-    godfather_victim = st.text_input(f"Enter Victim's Name (if applicable) (Night {night}):")
-    matador_victim = st.text_input(f"Enter Matador's Target's Name (if applicable) (Night {night}):")
-
-    # Button to display night results for the current night
-    if st.button(f"Night Result {night}"):
-        night_data = {
-            "Godfather Ability": godfather_ability,
-            "Godfather Victim": godfather_victim,
-            "Matador Victim": matador_victim
-        }
-
-        # Store night-specific data
-        st.session_state.night_data[night] = night_data
-
-        display_night_results(night)
-
 def display_night_results(night):
     night_data = st.session_state.night_data.get(night, {})
     godfather_ability = night_data.get("Godfather Ability", "doesn't kill anyone")
     godfather_victim = night_data.get("Godfather Victim", "")
     matador_victim = night_data.get("Matador Victim", "")
-    matador_ability_message = f"The Matador took the ability of {matador_victim}, who cannot use their ability." if matador_victim else ""
+    matador_ability_message = ""
+
+    if matador_victim:
+        matador_role = get_person_role_by_name(matador_victim)
+        matador_ability_message = f"The Matador ({matador_role}) took the ability of {matador_victim}, who cannot use their ability."
 
     night_actions = ""
 
@@ -139,6 +39,3 @@ def display_night_results(night):
     night_actions += f"\n{matador_ability_message}"
 
     st.write(night_actions)
-
-if __name__ == "__main__":
-    main()
